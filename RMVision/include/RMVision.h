1468f2da85af5b6dd4876f0e22d4697d312757ed
Peter Seid <pwseid@gmail.com>
2026-02-27 23:48:08 -0800

    SPM: export RMVision module headers without OpenCV-heavy includes

--- RMVision/include/RMVision.h
+++ RMVision/include/RMVision.h
@@ -1,19 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-//                             _
-//                            | | (_)
-//   _ __ ___  _ __ ___   ___ | |_ ___   _____
-//  | '__/ _ \| '_ ` _ \ / _ \| __| \ \ / / _ \
-//  | | | (_) | | | | | | (_) | |_| |\ V /  __/
-//  |_|  \___/|_| |_| |_|\___/ \__|_| \_/ \___|
-//
-////////////////////////////////////////////////////////////////////////////////
-//
 //  RMVision.h
-//  RMVision
-//
-//  Created by Romotive on 10/26/12.
-//  Copyright (c) 2012 Romotive. All rights reserved.
-//
 ////////////////////////////////////////////////////////////////////////////////
 #import <AVFoundation/AVFoundation.h>
 #import <UIKit/UIKit.h>
@@ -24,29 +10,8 @@
 @class RMVisionModule;
 @class RMVisionTrainingData;
 
-/**
- Simple data type to indicate camera types
- */
-typedef enum {
-    /// Indicates the back camera
-    RMCamera_Back    = 0,
-    /// Indicates the front-facing camera
-    RMCamera_Front   = 1
-} RMCamera;
-
-/**
- Simple data type to indicate quality of camera capture 
- */
-typedef enum {
-    /// High
-    RMCameraQuality_High,
-    /// Low
-    RMCameraQuality_Low
-} RMCameraQuality;
-
-/**
- A state of normalized brightness for the current vision feed
- */
+typedef enum { RMCamera_Back = 0, RMCamera_Front = 1 } RMCamera;
+typedef enum { RMCameraQuality_High, RMCameraQuality_Low } RMCameraQuality;
 typedef enum {
     RMVisionBrightnessStateUnknown   = 0,
     RMVisionBrightnessStateTooDark   = 1,
@@ -55,27 +20,16 @@ typedef enum {
     RMVisionBrightnessStateTooBright = 4,
 } RMVisionBrightnessState;
 
-/**
- The pre-built modules
- */
 extern NSString *const RMVisionModule_TakePicture;
 extern NSString *const RMVisionModule_TakeVideo;
 extern NSString *const RMVisionModule_FaceDetection;
 extern NSString *const RMVisionModule_EyeDetection;
 extern NSString *const RMVisionModule_GPUImageExample;
 
-// Forward definition
 @protocol RMVisionDelegate;
 
-/**
- Captures video from an iOS camera and delegates the frame to an arbitrary
- number of vision modules
- */
 @interface RMVision : NSObject
-
-// Capture properties
 @property (nonatomic) RMCamera camera;
-
 @property (nonatomic) RMCameraQuality quality;
 @property (nonatomic, readonly) float width;
 @property (nonatomic, readonly) float height;
@@ -84,149 +38,65 @@ extern NSString *const RMVisionModule_GPUImageExample;
 @property (atomic, readonly, getter=isRunning) BOOL running;
 @property (nonatomic, getter=isImageFlipped) BOOL imageFlipped;
 @property (nonatomic, getter=isAudioEnabled) BOOL audioEnabled;
-
 @property (nonatomic) int targetFrameRate;
-
-// Focus and exposure point of interest are in Romo coordinate system
-// (0,0) center of image
-// (-1, -1) top-left of image
-// (1, 1) bottom-right of image
 @property (nonatomic) CGPoint focusPointOfInterest;
 @property (nonatomic) CGPoint exposurePointOfInterest;
-
-// Performance
 @property (atomic, readonly) float fps;
-
-// Delegate
 @property (nonatomic, weak) id <RMVisionDelegate> delegate;
-
-// AVFoundation components
-@property (nonatomic, readonly) AVCaptureSession            *session;
-@property (nonatomic, readonly) AVCaptureDevice             *device;
-@property (nonatomic, readonly) AVCaptureVideoDataOutput    *videoOutput;
-@property (nonatomic, readonly) AVCaptureVideoPreviewLayer  *videoPreviewLayer;
-
-// Initialization
-//------------------------------------------------------------------------------
+@property (nonatomic, readonly) AVCaptureSession *session;
+@property (nonatomic, readonly) AVCaptureDevice *device;
+@property (nonatomic, readonly) AVCaptureVideoDataOutput *videoOutput;
+@property (nonatomic, readonly) AVCaptureVideoPreviewLayer *videoPreviewLayer;
 - (id)init;
-
 - (id)initWithCamera:(RMCamera)camera;
-
-- (id)initWithCamera:(RMCamera)camera
-          andQuality:(RMCameraQuality)quality;
-
-// Capturing
-//------------------------------------------------------------------------------
+- (id)initWithCamera:(RMCamera)camera andQuality:(RMCameraQuality)quality;
 - (void)startCapture;
 - (void)startCaptureWithCompletion:(void (^)(BOOL didSuccessfullyStart))completion;
 - (void)stopCapture;
 - (void)stopCaptureWithCompletion:(void (^)(BOOL didSuccessfullyStop))completion;
-
 - (UIImage *)currentImage;
-
-// Modules
-
-/**
- A set of active modules that conform to the RMVisionModuleProtocol
- */
 @property (nonatomic, strong, readonly) NSSet *activeModules;
-
-/**
- Given an object that conforms to the RMVisionModuleProtocol
- Registers it with this RMVision instance and provides frames when not paused
- */
 - (void)activateModule:(id<RMVisionModuleProtocol>)module;
-
-/** 
- Unregisters the given module and stops providing frames
- */
 - (void)deactivateModule:(id<RMVisionModuleProtocol>)module;
-
 - (void)deactivateAllModules;
-
 - (BOOL)activateModuleWithName:(NSString *)moduleName;
 - (BOOL)deactivateModuleWithName:(NSString *)moduleName;
-
 - (BOOL)pauseModuleWithName:(NSString *)moduleName;
 - (BOOL)unpauseModuleWithName:(NSString *)moduleName;
-
-// Training
-//------------------------------------------------------------------------------
-- (BOOL)trainModule:(NSString *)moduleName
-           withData:(id)trainingData;
-
+- (BOOL)trainModule:(NSString *)moduleName withData:(id)trainingData;
 @end
 
 @protocol RMVisionDelegate <NSObject>
-
 @optional
-
-// Face detection
-//------------------------------------------------------------------------------
-/**
- Delegate method that is triggered when a face is detected
- */
 - (void)didDetectFace:(RMFace *)face;
-
-/**
- Triggered when a face is lost
- */
 - (void)didLoseFace;
-
-// Motion detection
-//------------------------------------------------------------------------------
-/**
- Delegate method that is triggered when motion is detected
- */
 - (void)didDetectMotion:(RMMotion *)motion;
-
-/**
- Triggered when motion is lost
- */
 - (void)didLoseMotion;
-
-// Blob Detection
-//------------------------------------------------------------------------------
-/**
- Delegate method that is triggered when a brightly colored blob is detected
- */
 - (void)didDetectBlob:(RMBlob *)blob;
-
-/**
- Triggered when a blob is lost
- */
 - (void)didLoseBlob;
-
-// Line Detection
-//------------------------------------------------------------------------------
-/**
- Delegate method that is triggered when a line is detected
- */
 - (void)didDetectLine:(RMLine *)line;
-
-/**
- Triggered when the line is lost
- */
 - (void)didLoseLine:(RMLine *)line;
-
-// Color Detection
-//------------------------------------------------------------------------------
-/**
- Delegate method that is triggered when one or more bright colors are detected
- */
 - (void)didDetectColors:(RMColors *)colors;
-
-/**
- Triggered when no bright colors are detected
- */
 - (void)didLoseColors;
-
-
-// Debug
-//------------------------------------------------------------------------------
-/**
- Triggered when a debug image is available
- */
 - (void)showDebugImage:(UIImage *)debugImage;
-
 @end
+
+#import "RMBorderView.h"
+#import "RMBrightnessMeteringModule.h"
+#import "RMEyeDetectionModule.h"
+#import "RMFaceDetectionModule.h"
+#import "RMGPUImageExampleModule.h"
+#import "RMImageUtils.h"
+#import "RMLineDetectionModule.h"
+#import "RMMotionDetectionModule.h"
+#import "RMMotionTriggeredColorTrainingModule.h"
+#import "RMPictureModule.h"
+#import "RMThroughRomosEyesModule.h"
+#import "RMVideoModule.h"
+#import "RMVisualStasisDetectionModule.h"
+#import "RMVisionBrightHueSegmentationModule.h"
+#import "RMVisionDebugBroker.h"
+#import "RMVisionModule.h"
+#import "RMVisionNaiveLineTrainingModule.h"
+#import "RMVisionObjectTrackingModule.h"
+#import "RMVisionObjectTrackingModuleDebug.h"
